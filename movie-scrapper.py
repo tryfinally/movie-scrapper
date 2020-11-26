f059fa9e199d8d85bf9d2fcb513a08347fc86a43
Muhammad Zahalqa <m@tryfinally.com>
2020-11-26 10:55:29 +0200

    intial scraper

--- movie-scrapper.py
+++ movie-scrapper.py
@@ -1,10 +1,68 @@
 #!/usr/bin/env python3
 
+import sys
 import argparse
+import requests
+from bs4 import BeautifulSoup
+
+def prepare_search_url(args):
+    query = " ".join(args.search_term)
+    query_type = "ft" if args.by_movie_title else "tv"
+    return "https://www.imdb.com/find?q={}&s=tt&ttype={}".format(query, "ft")
+
+def retrieve_details_for(title, title_url):
+    url = "https://www.imdb.com" + title_url
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    title_div = soup.find('div', class_='title_wrapper')
+    title = title_div.find('h1').text
+    length = soup.find('time').text
+    subtext = soup.find('div', class_='subtext')
+    subtext_items = subtext.text.split('|')
+    # print(subtext_items)
+    credit_summary_items = soup.find_all('div', class_='credit_summary_item')
+
+    directors = credit_summary_items[0].find_all('a')
+    directors_names = ", ".join([d.text for d in directors])
+
+    #  ignore last a href is for full cast
+    stars = credit_summary_items[2].find_all('a')[:-1]
+    stars_names = ", ".join([d.text for d in stars])
+
+    print(title)
+    print("Director: ", directors_names)
+    print("Stars: ", stars_names)
+    print("Time: ", length.strip())
+    print("Rating: ", subtext_items[0].strip())
+    print("----"*10)
+
+
+
+
+
+def retrieve_records_for(results):
+    for result in results:
+        record = result.find('td', class_='result_text')
+        title = record.text
+        titl_url = record.find('a')['href']
+        retrieve_details_for(title, titl_url)
+
+def queryIMDb(args):
+    url = prepare_search_url(args)
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    main_response = soup.find(id='main')
+    # print(results.prettify())
+
+    list = main_response.find_all('table', class_='findList')
+    results = list[0].find_all('tr', class_='findResult')
+
+    retrieve_records_for(results)
 
 def main():
     parser = argparse.ArgumentParser(description='IMDb scraper')
-    parser.add_argument('search_term', action='store',
+    parser.add_argument('search_term', action='store', nargs='*',
                         help='Search term to query IMDb. like word/s in title')
 
 
@@ -14,7 +72,11 @@ def main():
                             help='Search by TV title.')
 
     args = parser.parse_args()
+    if len(args.search_term) == 0:
+        print("you must specify a search term")
+        exit(1)
 
+    queryIMDb(args)
 
 if __name__ == "__main__":
     main()
